260ef46f1022efcd5d5a3895d95a1fa0d3051f7b
arhariri <ali.71hariri@gmail.com>
2017-10-17 15:11:24 +0330

    close #1 Change secret from not null constraint to no constraint Add is_user column to person table to differentiate between user and person that is not user Back is_admin to user model

--- sql/person/create.sql
+++ sql/person/create.sql
@@ -3,6 +3,7 @@ create table person(
     firstname varchar(20),
     surename varchar(20),
     username varchar(25) not null unique,
-    secret varchar(200) not null,
+    secret varchar(200),
+    is_user boolean default true,
     display_name varchar(30)
 );
