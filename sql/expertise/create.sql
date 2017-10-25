cf63cdca13fd2e3dfb6db4066537ac704dbcd33b
arhariri <ali.71hariri@gmail.com>
2017-10-25 13:05:02 +0330

    regular user and admin can add expertise and update it

--- sql/expertise/create.sql
+++ sql/expertise/create.sql
@@ -1,8 +1,8 @@
 create table if not exists expertise(
     expertise_id serial not null primary key,
-    name_en varchar(30) not null,
-    name_fa varchar(30) not null,
-    type_en varchar(20) not null,
-    type_fa varchar(20) not null,
+    name_en varchar(100) not null,
+    name_fa varchar(100) not null,
+    type_en varchar(50) not null,
+    type_fa varchar(50) not null,
     is_education boolean not null
 );
