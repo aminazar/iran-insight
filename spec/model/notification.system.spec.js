1ebdadd9de7e878f9ca650522dc6e9fa6349f563
Amin Azarbadegan <amin@azarbadegan.net>
2017-11-13 11:42:59 +0330

    Changing logic to keep messages after publishing

--- spec/model/notification.system.spec.js
+++ spec/model/notification.system.spec.js
@@ -8,7 +8,7 @@ describe("Notification System", () => {
     notificationSystem.setup(true)
       .then(() => {
         ns = notificationSystem.get();
-        return redis.redis_client().keysAsync(`${ns.pattern}:*`);
+        return redis.redis_client().keysAsync(`${ns.pattern}*`);
       })
       .then(keys => {
         return Promise.all(keys.map(k => redis.redis_client().delAsync(k)));
@@ -22,31 +22,37 @@ describe("Notification System", () => {
   });
 
   it('should publish/subscribe a notification', function (done) {
-    ns.subscribe(10,
+    let o = ns.getObservable(10).subscribe(
       d => {
         expect(d.x).toBe(1);
         expect(d.y.z).toBe(2);
-        ns.deleteChannel(10);
-        done();
+        ns.deleteNotification(10, d.order)
+          .then(()=>{
+            o.unsubscribe();
+            done();
+          })
+
       }, err => console.log(err));
 
     ns.pushNotification({x: 1, y: {z: 2}}, {pid: 10});
   });
 
   it('should reuse the channel', function (done) {
-    ns.subscribe(10,
+    let o = ns.getObservable(10).subscribe(
       d => {
         expect(d.x).toBe(2);
         expect(d.y.z).toBe(3);
-        ns.deleteChannel(10);
-        done()
+        ns.deleteNotification(10, d.order)
+          .then(()=>{
+            o.unsubscribe();
+            done();
+          })
       }, err => console.log(err));
 
     ns.pushNotification({x: 2, y: {z: 3}}, {pid: 10});
   });
 
   it('should queue messages for fetching', function (done) {
-    ns.deleteChannel(10);
     let range = [];
     for (let i = 0; i < 100; i++)
       range.push(i);
@@ -57,16 +63,45 @@ describe("Notification System", () => {
         if (res[10]) {
           expect(res[10].x).toBe(89)
         }
-        done();
       })
+      .then(() => ns.deleteNotification(10))
+      .then(done)
       .catch(err => console.log(err));
   });
 
+  it('should keep message even after publishing it', function (done) {
+    let o = ns.getObservable(10).subscribe( d => {
+      expect(d.x).toBeLessThan(6);
+      if (d.x === 5) {
+        o.unsubscribe();
+      }
+    });
+
+    let i = 0;
+    let s = setInterval(() => {
+      ns.pushNotification({x: i++}, {pid: 10});
+      if (i >= 10)
+        clearInterval(s);
+    }, 10);
+
+    setTimeout(() => ns.fetchNotifications(10)
+      .then(res => {
+        expect(res.length).toBe(10);
+        if (res[10]) {
+          expect(res[4].x).toBe(6);
+        }
+      })
+      .then(() => ns.deleteNotification(10))
+      .then(done)
+      , 1000);
+  });
+
   it('should work with both queue and live subscription', function (done) {
-    ns.subscribe(10, d => {
+    let o = ns.getObservable(10).subscribe( d => {
       expect(d.x).toBeLessThan(11);
       if (d.x === 10) {
-        ns.deleteChannel(10);
+        o.unsubscribe();
+        ns.deleteNotification(10);
       }
     });
 
@@ -84,7 +119,8 @@ describe("Notification System", () => {
         if (res[10]) {
           expect(res[10].x).toBe(90);
         }
-      }), 2000);
+      })
+      .then(() => ns.deleteNotification(10)), 2000);
 
     setTimeout(() => ns.fetchNotifications(10)
       .then(res => {
@@ -93,46 +129,34 @@ describe("Notification System", () => {
       }), 3000);
   });
 
-  it('should unsubscribe all subscribtions after deleting channel', function (done) {
-    ns.subscribe(10, d => fail('it did not unsubscribe'));
-    ns.subscribe(10, d => fail('it did not unsubscribe this one too!'));
-
-    ns.deleteChannel(10);
-    ns.pushNotification({}, {pid: 10})
-      .then(()=> ns.fetchNotifications(10))
-      .then(res => {
-        expect(res.length).toBe(1);
-        expect(res[0]).toBeTruthy();
-        expect(res[0].order).toBeTruthy();
-        expect(res[0].timestamp).toBeTruthy();
-        setTimeout(done, 100);
-      })
-  });
-
   it('should work with multiple channels', function (done) {
-    ns.subscribe(10, d => expect(d.data).toBe(11));
-    ns.subscribe(11, d => expect(d.data).toBe(10));
+    let o1 = ns.getObservable(10).subscribe(d => {
+        expect(d.data).toBe(11);
+        o1.unsubscribe();
+      }),
+        o2 = ns.getObservable(11).subscribe(d => {
+          expect(d.data).toBe(10);
+          o2.unsubscribe();
+        });
+
     ns.pushNotification({data: 11}, {pid: 10})
       .then(() => ns.pushNotification({data:10}, {pid: 11}))
-      .then(() => {
-        setTimeout(() => {
-          ns.deleteChannel(10);
-          ns.deleteChannel(11);
-          done();
-        })
-      });
+      .then(() => ns.deleteNotification(10))
+      .then(() => ns.deleteNotification(11))
+      .then(done);
   });
 
   it('should send actionable notification to org rep', function (done) {
     spyOn(sql.test.membership, 'getOrgRep').and.returnValue(Promise.resolve(
       [{pid:12}]));
 
-    ns.subscribe(12, d => {
+    let o = ns.getObservable(12).subscribe(d => {
       expect(d.isActionable).toBe(true);
       expect(d.data).toBe('xyz');
       expect(sql.test.membership.getOrgRep).toHaveBeenCalledWith({oid:2});
-      ns.deleteChannel(12);
-      done()
+      o.unsubscribe();
+      ns.deleteNotification(12)
+        .then(done);
     });
 
     ns.pushNotification({data:'xyz'}, {oid:2})
@@ -145,12 +169,13 @@ describe("Notification System", () => {
     spyOn(sql.test.membership, 'getBizRep').and.returnValue(Promise.resolve(
       [{pid:12}]));
 
-    ns.subscribe(12, d => {
+    let o = ns.getObservable(12).subscribe(d => {
       expect(d.isActionable).toBe(true);
       expect(d.data).toBe('xyz');
       expect(sql.test.membership.getBizRep).toHaveBeenCalledWith({bid:2});
-      ns.deleteChannel(12);
-      done()
+      o.unsubscribe();
+      ns.deleteNotification(12)
+        .then(done);
     });
 
     ns.pushNotification({data:'xyz'}, {bid:2})
@@ -163,12 +188,13 @@ describe("Notification System", () => {
     spyOn(sql.test.subscription, 'getOrgSubscribers').and.returnValue(Promise.resolve(
       [{pid:12},{pid:15}]));
 
-    ns.subscribe(15, d => {
+    let o = ns.getObservable(15).subscribe(d => {
       expect(d.isActionable).toBe(undefined);
       expect(d.data).toBe('xyz');
       expect(sql.test.subscription.getOrgSubscribers).toHaveBeenCalledWith({oid:2});
-      ns.deleteChannel(15);
-      done()
+      o.unsubscribe();
+      ns.deleteNotification(15)
+        .then(done);
     });
 
     ns.pushNotification({data:'xyz', from: {oid:2}})
@@ -181,12 +207,13 @@ describe("Notification System", () => {
     spyOn(sql.test.subscription, 'getBizSubscribers').and.returnValue(Promise.resolve(
       [{pid:12},{pid:15}]));
 
-    ns.subscribe(15, d => {
+    let o = ns.getObservable(15).subscribe(d => {
       expect(d.isActionable).toBe(undefined);
       expect(d.data).toBe('xyz');
       expect(sql.test.subscription.getBizSubscribers).toHaveBeenCalledWith({bid:2});
-      ns.deleteChannel(15);
-      done()
+      o.unsubscribe();
+      ns.deleteNotification(15)
+        .then(done);
     });
 
     ns.pushNotification({data:'xyz', from:{bid:2}})
@@ -199,12 +226,13 @@ describe("Notification System", () => {
     spyOn(sql.test.subscription, 'getPersonSubscribers').and.returnValue(Promise.resolve(
       [{pid:12},{pid:15}]));
 
-    ns.subscribe(15, d => {
+    let o = ns.getObservable(15).subscribe(d => {
       expect(d.isActionable).toBe(undefined);
       expect(d.data).toBe('xyz');
       expect(sql.test.subscription.getPersonSubscribers).toHaveBeenCalledWith({pid:2});
-      ns.deleteChannel(15);
-      done()
+      o.unsubscribe();
+      ns.deleteNotification(15)
+        .then(done);
     });
 
     ns.pushNotification({data:'xyz', from: {pid:2}})
