e4584ace6c68a59a06e707a8f96a6e81ce22945b
Eabasir <eabasir@gmail.com>
2018-01-01 15:20:00 +0330

    add tag_connection and its api to server to handle relations and affinities in tags

--- spec/api/tag/get.spec.js
+++ spec/api/tag/get.spec.js
@@ -56,10 +56,10 @@ describe("Get tag", () => {
         return sql.test.product.add({name: 'android app', business_id: biz.bid})
       })
       .then(res => {
-        return sql.test.tag.add({name: 'اینترنت', proposer: {business: [biz.bid], organization: [], product: []}})
+        return sql.test.tag.add({name: 'اینترنت', active: true})
       })
       .then(res => {
-        return sql.test.tag.add({name: 'حمل و نقل'})
+        return sql.test.tag.add({name: 'حمل و نقل', active: false})
       })
       .then(() => {
         done();
@@ -73,8 +73,8 @@ describe("Get tag", () => {
 
   it("every user should be able to get activated tags", function (done) {
     this.done = done;
-    sql.test.tag.appendTag({tableName: 'business', tag: 'اینترنت', condition: `bid = ${biz.bid}`})
-      .then(res => sql.test.tag.appendTag({
+    sql.test.tag.appendTagToTarget({tableName: 'business', tag: 'اینترنت', condition: `bid = ${biz.bid}`})
+      .then(res => sql.test.tag.appendTagToTarget({
         tableName: 'business',
         tag: 'حمل و نقل',
         condition: `bid = ${biz.bid}`
@@ -89,14 +89,12 @@ describe("Get tag", () => {
 
           expect(res.statusCode).toBe(200);
           let result = JSON.parse(res.body);
-
           expect(result[0].gettags.length).toBe(1);
           done();
         })
         .catch(lib.helpers.errorHandler.bind(this)))
 
   });
-
 
 
 });
--- sql/tag/removeTagFromTarget.sql
+++ sql/tag/removeTagFromTarget.sql
@@ -1,13 +1,1 @@
-do $$
-declare
-    tag_id int;
-begin
-
-    UPDATE ${tableName~} SET tags = array_remove(tags,${tag}) WHERE ${condition^};
-    select tid into tag_id from tag where name like '%${tag^}%';
-
-     UPDATE tag
-        SET proposer = jsonb_set(proposer,'{${tableName^}}', (proposer->${tableName}) - ${proposerId}, true) -- table name here represents proposer type
-     where tid = tag_id;
-
-end $$;
+UPDATE ${tableName~} SET tags = array_remove(tags,${tag}) WHERE ${condition^};
