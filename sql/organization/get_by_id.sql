8445c6814704e843b1e659dcafd3c4cb26985166
arhariri <ali.71hariri@gmail.com>
2018-01-10 17:50:42 +0330

    Change get query to get specific columns from organization table

--- sql/organization/get_by_id.sql
+++ sql/organization/get_by_id.sql
@@ -1,13 +1,16 @@
 SELECT
-o.oid,
-o.name as org_name,
-o.name_fa as org_name_fa,
-o.start_date as org_start_date,
-o.end_date as org_end_date,
-ot.name as org_type,
-ot.name_fa as org_type_fa
-
+    o.oid,
+    o.name as org_name,
+    o.name_fa as org_name_fa,
+    o.start_date as org_start_date,
+    o.end_date as org_end_date,
+    ot.id as org_type_id,
+    ot.name as org_type,
+    ot.name_fa as org_type_fa,
+    person.pid as ceo_id,
+    person.display_name_en as ceo_name,
+    person.display_name_fa as ceo_name_fa
 FROM organization as o
-LEFT JOIN organization_type as ot
-ON o.org_type_id = ot.id
-where o.oid = ${oid};
+LEFT outer JOIN organization_type as ot ON o.org_type_id = ot.id
+left outer join person on o.ceo_pid = person.pid
+where o.oid = ${oid}
