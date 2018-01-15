bb3bb1bf966f209e18f6bbe468b6731603f0b207
arhariri <ali.71hariri@gmail.com>
2018-01-15 15:51:55 +0330

    remove path from hard conding in ex_scripts file

--- ex_scripts.js
+++ ex_scripts.js
@@ -1,7 +1,5 @@
 const externalData = require('./lib/externalData.model');
-const stdin = process.openStdin();
+const args = process.argv.slice(2);
 
 let exData = new externalData();
-
-//Should declared the excel file path
-exData.readSpreadSheet('c://users//ali71//desktop//t.xlsx');
+exData.readSpreadSheet(args[0]);
