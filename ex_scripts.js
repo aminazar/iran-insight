571b0f29c9eae155482626a148cec703ee8fe1a5
arhariri <ali.71hariri@gmail.com>
2018-01-28 18:55:14 +0330

    fix bug in ex_script file

--- ex_scripts.js
+++ ex_scripts.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 
 let exData = new externalData();
 
-if(args[0].toLowerCase() === 'r' || args[0].toLowerCase() === 'read')
+if(args[1].toLowerCase() === 'r' || args[1].toLowerCase() === 'read')
   exData.readSpreadSheet(args[0]);
-else if(args[0].toLowerCase() === 'w' || args[0].toLowerCase() === 'write')
+else if(args[1].toLowerCase() === 'w' || args[1].toLowerCase() === 'write')
   exData.saveToSpreadSheet(args[0]);
