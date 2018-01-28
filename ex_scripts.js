bb633e311be5fc9bf28a21294c5ea58b0d566680
arhariri <ali.71hariri@gmail.com>
2018-01-28 18:50:31 +0330

    exporting ex_data records into excel file

--- ex_scripts.js
+++ ex_scripts.js
@@ -2,4 +2,8 @@ const externalData = require('./lib/externalData.model');
 const args = process.argv.slice(2);
 
 let exData = new externalData();
-exData.readSpreadSheet(args[0]);
+
+if(args[0].toLowerCase() === 'r' || args[0].toLowerCase() === 'read')
+  exData.readSpreadSheet(args[0]);
+else if(args[0].toLowerCase() === 'w' || args[0].toLowerCase() === 'write')
+  exData.saveToSpreadSheet(args[0]);
